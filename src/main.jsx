47c2886c8dfcd33fb5cd437a840b46ed1816e7ab
Ferney Segura <ferneysegura@Fers-MacBook-Pro.local>
2022-10-20 01:59:16 -0500

    Before merge

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import {
 } from "react-router-dom";
 
 const client = new ApolloClient({
-	uri: "http://localhost:5000/graphql",
+	uri: "http://localhost:5001/graphql",
 	
 	cache: new InMemoryCache(),
 });
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -3,32 +3,23 @@ import { useQuery } from "@apollo/client";
 import { GET_COURSES, GET_COURSES_BY_ID } from "../features/courses/courses.querys";
 
 const Courses = () => {
-	const { loading, error, data } = useQuery(GET_COURSES_BY_ID, {
-		variables: { id: "0005" },
-	});
+	const result = useQuery(GET_COURSES);
 
-	// const [example, setExample] = useState(null);
+	console.log(result);
 
-	// useEffect(() => {
-	// 	document.title = `${example}`;
-	//   }, [example]);
-
-	console.log(data);
-
-	if (loading) return <div>Loading...</div>;
-	if (error) return <div>Error {error}</div>;
+	// if (loading) return <div>Loading...</div>;
+	// if (error) return <div>Error {error}</div>;
 
 	return (
 		<>
 			<div>
-				<input onChange={(e) => setExample(e.target.value)}></input>
 				<div className="title">Cursos</div>
 				<h1>Hola</h1>
-				{data.getCursosById.map((item) => (
+				{/* {data.getCursosById.map((item) => (
 					<div key={item.id} className='courses-row'>
 						<span>{item.id}</span>
 					</div>
-				))}
+				))} */}
 			</div>
 		</>
 	);
