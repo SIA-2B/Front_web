c6df061106d139e4d009f3ddfe3dc7b74841e222
Ferney Segura <ferneysegura@Fers-MacBook-Pro.local>
2022-10-28 23:19:38 -0500

    Some styles

--- src/components/CoursesA.jsx
+++ src/components/CoursesA.jsx
@@ -1,129 +1,97 @@
 import React, { useState, useEffect } from "react";
 import { useSelect } from "../context/SelectContext";
 import { GET_COURSES_BY_PLAN } from "../features/courses/courses.querys";
-import { useLazyQuery,useQuery } from "@apollo/client";
+import { useLazyQuery, useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 
-export const CoursesA=({cursos}) =>{
-	if (cursos==null) return null
+export const CoursesA = ({ cursos }) => {
+	if (cursos == null) return null;
 
 	const a = useSelect();
 
-	const caca =a.cur.data?.allPlanes.filter(sup=> sup.nombre==a.selectCarrera)
+	const caca = a.cur.data?.allPlanes.filter(
+		(sup) => sup.nombre == a.selectCarrera
+	);
 
+	var arr = [];
 
-
-
-	var arr =[]
-
-	
-
-	if(a.selectCarrera=="Any"){
+	if (a.selectCarrera == "Any") {
 		cursos.forEach(function (e) {
-
 			if (
-			((e.id ==a.selectId) || (a.selectId=="Any") )   &&    
-			((e.nombre ==a.selectNombre) || (a.selectNombre=="Any"))    &&    
-			((e.creditos ==a.selectCreditos) || (a.selectCreditos=="Any") )    &&    
-			((e.tipologia ==a.selectTipologia) || (a.selectTipologia=="Any"))   &&
-			((e.sede ==a.selectSede) || (a.selectSede=="Any"))   &&
-			((e.nivelestudio ==a.selectNivelestudio) || (a.selectNivelestudio=="Any"))   &&
-			((e.facultad ==a.selectFacultad) || (a.selectFacultad=="Any")) 
-			
+				(e.id == a.selectId || a.selectId == "Any") &&
+				(e.nombre == a.selectNombre || a.selectNombre == "Any") &&
+				(e.creditos == a.selectCreditos || a.selectCreditos == "Any") &&
+				(e.tipologia == a.selectTipologia || a.selectTipologia == "Any") &&
+				(e.sede == a.selectSede || a.selectSede == "Any") &&
+				(e.nivelestudio == a.selectNivelestudio ||
+					a.selectNivelestudio == "Any") &&
+				(e.facultad == a.selectFacultad || a.selectFacultad == "Any")
 			) {
 				arr.push(e);
 			}
-			
 		});
-	}
-	else{
-		const cursosByPlan  = useQuery(GET_COURSES_BY_PLAN, {
-			variables:{ idPlan: caca[0].codigo},
+	} else {
+		const cursosByPlan = useQuery(GET_COURSES_BY_PLAN, {
+			variables: { idPlan: caca[0].codigo },
 		});
-	
-		//const result = cursosByPlan.data.cursosByPlan[0].id
-		
-	
-		var cursosPorPlan = cursosByPlan.data?.cursosByPlan
-		console.log(cursosByPlan.data?.cursosByPlan,"Putos")
+		var cursosPorPlan = cursosByPlan.data?.cursosByPlan;
+		console.log(cursosByPlan.data?.cursosByPlan, "Putos");
 
-		
-		if(cursosPorPlan==undefined){
-			return(
-				<div>No Hay Coincidencias</div>
-			)
+		if (cursosPorPlan == undefined) {
+			return <div>No Hay Coincidencias</div>;
 		}
 
 		cursosPorPlan.forEach(function (e) {
-
 			if (
-			((e.id ==a.selectId) || (a.selectId=="Any") )   &&    
-			((e.nombre ==a.selectNombre) || (a.selectNombre=="Any"))    &&    
-			((e.creditos ==a.selectCreditos) || (a.selectCreditos=="Any") )    &&    
-			((e.tipologia ==a.selectTipologia) || (a.selectTipologia=="Any"))   &&
-			((e.sede ==a.selectSede) || (a.selectSede=="Any"))   &&
-			((e.nivelestudio ==a.selectNivelestudio) || (a.selectNivelestudio=="Any"))   &&
-			((e.facultad ==a.selectFacultad) || (a.selectFacultad=="Any")) 
-			
+				(e.id == a.selectId || a.selectId == "Any") &&
+				(e.nombre == a.selectNombre || a.selectNombre == "Any") &&
+				(e.creditos == a.selectCreditos || a.selectCreditos == "Any") &&
+				(e.tipologia == a.selectTipologia || a.selectTipologia == "Any") &&
+				(e.sede == a.selectSede || a.selectSede == "Any") &&
+				(e.nivelestudio == a.selectNivelestudio ||
+					a.selectNivelestudio == "Any") &&
+				(e.facultad == a.selectFacultad || a.selectFacultad == "Any")
 			) {
 				arr.push(e);
 			}
-			
 		});
 	}
 
-	
-
-
-	return(
+	return (
 		<div>
-			<table class="title3">
-			<tr>
-
-				<th class="title3">ID</th>
-
-				<th class="title3">Nombre</th>
-
-				<th class="title3">Creditos</th>
-
-				<th class="title3">Tipologia</th>
-
-				<th class="title3">Sede</th>
-
-				<th class="title3">NivelEstudio</th>
-
-				<th class="title3">Facultad</th>
-
-			</tr>
-
-			{arr.map(c=>  
-			
-			<tr key={c.id} >
-				
-				<Link to={"/groups"}
-				onClick={(e) => {
-					a.idGrupo=c.id
-					
-				}}>
-				<th class="title3">{c.id}</th>
-				</Link>
-				<th class="title3">{c.nombre}</th>
-
-				<th class="title3">{c.creditos}</th>
-
-				<th class="title3">{c.tipologia}</th>
-
-				<th class="title3">{c.sede}</th>
-
-				<th class="title3">{c.nivelestudio}</th>
-
-				<th class="title3">{c.facultad}</th>
-
-			</tr>
-
-			)}
-
-			</table>
+			<h1>Resultados de la Busqueda</h1>
+			<div className="tbl-header">
+				<table cellPadding="0" cellSpacing="0" border="0">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Nombre</th>
+							<th>Creditos</th>
+							<th>Tipologia</th>
+							<th>Sede</th>
+							<th>NivelEstudio</th>
+							<th>Facultad</th>
+						</tr>
+					</thead>
+				</table>
+			</div>
+			<div className="tbl-content">
+				<table cellPadding="0" cellSpacing="0" border="0">
+					<tbody>
+						{arr.map((row, index) => (
+							<tr key={index}>
+								<td>{row.id.substring(1, 10)}</td>
+								<td>{row.nombre}</td>
+								<td>{row.creditos}</td>
+								<td>{row.tipologia}</td>
+								<td>{row.sede}</td>
+								<td>{row.nivelestudio}</td>
+								<td>{row.facultad}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
-	)
-}
+	);
+};
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,130 +1,48 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useLazyQuery,useQuery } from "@apollo/client";
+import { useLazyQuery, useQuery } from "@apollo/client";
 import {
 	GET_COURSES,
 	GET_COURSES_BY_ID,
 } from "../features/courses/courses.querys";
 import { CoursesA } from "../components/CoursesA";
-import { Opciones} from "../components/Selects";
-import { SelectProvider,useSelect } from "../context/SelectContext";
+import { Opciones } from "../components/Selects";
+import { SelectProvider, useSelect } from "../context/SelectContext";
 import "../styles/courses.css";
 
-
-
-
-
-
-
-import '../styles/courses.css'
+import "../styles/courses.css";
 
 const Courses = () => {
-
-	const {selectId} = useSelect();
+	const { selectId } = useSelect();
 	const [getCourse, { data, loading, error }] = useLazyQuery(GET_COURSES_BY_ID);
 
 	const cur = useQuery(GET_COURSES);
-	const [getAllCourse,result] = useLazyQuery(GET_COURSES);
+	const [getAllCourse, result] = useLazyQuery(GET_COURSES);
 
-
-	console.log(getAllCourse)
-	
+	console.log(getAllCourse);
 
 	return (
-
 		<div>
+			<h1>
+				Buscador de Cursos <br /> <br />{" "}
+			</h1>
 
-			<h2>Buscador de Cursos <br/> <br/> </h2>
-			
-			{ cur.data &&
-				<Opciones opc = {cur.data?.allCursos} />
-			}
+			{cur.data && <Opciones opc={cur.data?.allCursos} />}
 
-			{/* <input
-				value={courseId}
-				onChange={(e) => setCourseId(e.target.value)}
-			></input>
-			
+			<div>
+				<br />
+			</div>
 			<button
-				onClick={(e) => {
-					e.preventDefault();
-					getCourse({ variables: { id: courseId } });
-				}}
-				
-			>
-				Buscar
-			</button> */}
-			<div><br/></div>
-			<button
+			className="button-search"
 				onClick={(e) => {
 					e.preventDefault();
 					getAllCourse();
-					
 				}}
 			>
-				
-				<h3>Buscar</h3>
+				<h4>Buscar</h4>
 			</button>
 
-
-			<div>
-				{ result.data &&
-					<CoursesA cursos ={result.data?.allCursos}/>
-				}
-				
-			</div>
-			
-
-
-
-
-			{data && (
-				<div>
-					<h1></h1>
-					<h5>Curso Encontrado: </h5>
-					<div className="course-card">
-						<div className="info">
-							<div className="data">{data.cursosById.id}</div>
-							<div className="sub-title">ID</div>
-						</div>
-						<div className="info">
-							<div className="data">{data.cursosById.nombre}</div>
-							<div className="sub-title">Nombre Asignatura</div>
-						</div>
-						<div className="info">
-							<div className="data">{data.cursosById.creditos}</div>
-							<div className="sub-title">Creditos</div>
-						</div>
-						<div className="info">
-							<div className="data">{data.cursosById.tipologia}</div>
-							<div className="sub-title">Tipologia</div>
-						</div>
-						<div className="info">
-							<div className="data">{data.cursosById.sede}</div>
-							<div className="sub-title">Sede</div>
-						</div>
-						<div className="info">
-							<div className="data">{data.cursosById.nivel_estudio}</div>
-							<div className="sub-title">Nivel Estudio</div>
-						</div>
-						<div className="info">
-							<div className="data">{data.cursosById.facultad}</div>
-							<div className="sub-title">Facultad</div>
-						</div>
-						<div className="info">
-							<div className="data">{data.cursosById.descripcion}</div>
-							<div className="sub-title">Descripcion</div>
-						</div>
-						<div className="info">
-							<div className="data">{data.cursosById.codigo}</div>
-							<div className="sub-title">Código</div>
-						</div>
-					</div>
-				</div>
-			)}
+			<div>{result.data && <CoursesA cursos={result.data?.allCursos} />}</div>
 		</div>
 	);
 };
 export default Courses;
-
-
-
